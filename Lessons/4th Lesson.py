84fba1f38e2b1b966443e8dfeb7104bc3e13efdf
Carlos Barros <56875807+carlosefrbarros@users.noreply.github.com>
2023-02-27 14:56:35 -0300

    4th Lesson

---
+++ Lessons/4th Lesson.py
@@ -0,0 +1,47 @@
+print('Olá, Mundo!')
+
+print(7+4)
+
+print('7'+'4')
+
+print('Olá' + 5)
+
+print('Olá', 5)
+
+nome = 'Guanabara'
+
+idade = 25
+
+peso = 75.8
+
+print(nome, idade, peso)
+
+nome = input('Qual é o seu nome? ')
+
+idade = input('Quantos anos você tem? ')
+
+peso = input('Qual é o seu peso? ')
+
+print(nome, idade, peso)
+
+#exercise 01
+
+nome = input('Qual é o seu nome?')
+
+print('Olá ', nome, '! Prazer em te conhecer!')
+
+#exercise 02
+
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+
+print('Você nasceu no dia ', dia, 'de ', mes, 'de ', ano, '. Correto?')
+
+#exercise 03
+
+num1 = input('Número 1 = ')
+num2 = input('Número 2 = ')
+soma = int(num1) + int(num2)
+
+print('A soma de ', num1, ' e ', num2, ' é igual a ', soma, '.')
